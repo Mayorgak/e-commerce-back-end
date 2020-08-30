8999a49db6c73b55dc327a544023040fd4ac18cf
Kristen M <kristenmayorga@gmail.com>
2020-08-30 10:12:40 -0400

    updated apiroutes

--- models/Tag.js
+++ models/Tag.js
@@ -6,15 +6,27 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "tag",
-  }
+  tag_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate : {
+      len : [1]
+    }
+  },
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "tag",
+}
 );
 
 module.exports = Tag;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
+const { query } = require("express");
 
 // The `/api/products` endpoint
 
@@ -7,13 +8,49 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+
+   Product.findAll({
+     where :query,
+     include : [{
+       model :Category,  
+      },
+      {
+        model: Tag
+      }
+     ]
+   })
+  .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
 
 // get one product
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+    where :{
+      id: req.params.id
+    },
+    include : [{
+      model: Category,
+    },
+    {
+       model: Tag
+    }
+    ]
+  })
+  .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
+
 
 // create new product
 router.post("/", (req, res) => {
@@ -91,6 +128,10 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where : req.params.id
+  })
+  
 });
 
 module.exports = router;
