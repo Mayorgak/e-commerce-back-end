c803c4e5e039ebaf7a02fac5d845fa4be4c18db6
Kristen M <kristenmayorga@gmail.com>
2020-08-30 13:01:40 -0400

    adding missing code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -129,9 +129,21 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
   Product.destroy({
-    where : req.params.id
+    where: {
+      id: req.params.id,
+    },
   })
-  
+    .then((productData) => {
+      if (!productData) {
+        res.status(404).json({ message: "No Tag found with this id" });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
